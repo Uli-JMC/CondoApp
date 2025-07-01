7c7efa65c9191fe93af5132a689166b8025a8a4b
Ulises Monge C <umcalderon@gmail.com>
2025-07-01 14:05:17 -0600

    Creacion de Proyecto Condominio

---
+++ src/main/java/com/condoapp/CondoappApplication.java
@@ -0,0 +1,13 @@
+package com.condoapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CondoappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CondoappApplication.class, args);
+	}
+
+}
